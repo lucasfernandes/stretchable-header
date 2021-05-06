ec4bf21c54eab520699b726b517f15fe93ab5b0d
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2021-05-06 11:42:21 -0300

    Initial Commit

---
+++ StretchableHeader/StretchableHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchableHeaderApp.swift
+//  StretchableHeader
+//
+//  Created by Lucas Silveira on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct StretchableHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
